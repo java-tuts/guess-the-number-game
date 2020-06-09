c52c55152a0b5ea9e1b538084948f113a4813289
vighnesh153 <vighnesh.raut13@gmail.com>
2020-06-09 18:36:37 +0530

    add about using the bean lifecycle methods

--- core/src/main/java/com/vighnesh153/GameImpl.java
+++ core/src/main/java/com/vighnesh153/GameImpl.java
@@ -3,6 +3,9 @@ package com.vighnesh153;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class GameImpl implements Game {
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
@@ -15,11 +18,7 @@ public class GameImpl implements Game {
     private int remainingGuesses;
     private boolean validNumberRange = true;
 
-    // setter based DI
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }
-
+    @PostConstruct
     @Override
     public void reset() {
         smallest = 0;
@@ -28,6 +27,16 @@ public class GameImpl implements Game {
         biggest = numberGenerator.getMaxNumber();
         number = numberGenerator.next();
         log.debug("The number is {}", number);
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.info("in Game preDestroy()");
+    }
+
+    // setter based DI
+    public void setNumberGenerator(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
     }
 
     @Override
