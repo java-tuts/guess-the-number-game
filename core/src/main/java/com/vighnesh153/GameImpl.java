ff9abd64750f893f39f6765cae863279a506c221
vighnesh153 <vighnesh.raut13@gmail.com>
2020-06-09 18:49:47 +0530

    add about autowiring beans

--- core/src/main/java/com/vighnesh153/GameImpl.java
+++ core/src/main/java/com/vighnesh153/GameImpl.java
@@ -2,6 +2,7 @@ package com.vighnesh153;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -9,6 +10,7 @@ import javax.annotation.PreDestroy;
 public class GameImpl implements Game {
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 10;
     private int number;
@@ -32,11 +34,6 @@ public class GameImpl implements Game {
     @PreDestroy
     public void preDestroy() {
         log.info("in Game preDestroy()");
-    }
-
-    // setter based DI
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
     }
 
     @Override
