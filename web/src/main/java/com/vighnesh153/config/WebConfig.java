3c8ca5286300d0a1038ac6ded78e55be60816ae0
vighnesh153 <vighnesh.raut13@gmail.com>
2020-06-13 07:20:16 +0530

    add basics of interceptors

--- web/src/main/java/com/vighnesh153/config/WebConfig.java
+++ web/src/main/java/com/vighnesh153/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.vighnesh153.config;
 
+import com.vighnesh153.interceptor.RequestInterceptor;
 import com.vighnesh153.util.ViewNames;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 }
