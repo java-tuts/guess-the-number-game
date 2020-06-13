75f8d3e79e34d035a761d75f5de2d3af30ecc122
vighnesh153 <vighnesh.raut13@gmail.com>
2020-06-13 07:29:36 +0530

    add the locale change interceptor

--- web/src/main/java/com/vighnesh153/config/WebConfig.java
+++ web/src/main/java/com/vighnesh153/config/WebConfig.java
@@ -2,13 +2,22 @@ package com.vighnesh153.config;
 
 import com.vighnesh153.interceptor.RequestInterceptor;
 import com.vighnesh153.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
@@ -17,5 +26,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RequestInterceptor());
+
+        LocaleChangeInterceptor localeChangeInterceptor =
+                new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+
+        // for hindi: http://localhost:8080/play?lang=hi
+        // for english: http://localhost:8080/play?lang=en
+        // if locale code is not supported by our system, defaults to english
+
+        // If we didn't set the paramName to lang, the default name: 'locale'
+        // would have been considered
     }
 }
